71b57515702137b6d26a4c1e4b0558193a3a0526
eherrmann <emanuelherrmann@gmail.com>
2018-08-03 18:32:46 +1200

    Move helper scripts to root

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 # Assuming you deployed with a 'serverless deploy | tee deploy.out' this script will auto fill in the TODOS_ENDPOINT environment variable and execute your tests
-node ./helpers/getEndpoint.js > endpoint.out
+node getEndpoint.js > endpoint.out
 value=`cat endpoint.out`
 echo "Here's the endpoint: " $value
 export TODOS_ENDPOINT=$value
